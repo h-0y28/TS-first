8df11a6a2d29a60ebea03018126403ba6f880ad6
hyojoo <jhj080802@naver.com>
2024-08-19 01:37:03 +0900

    intersection

--- src/coding.ts
+++ src/coding.ts
@@ -1,46 +1,19 @@
-// literal types
-const userName1 = "Bob";
-let userName2: string | number = "Tom";
-userName2 = 3;
+// Intersection Types
 
-type Job = "police" | "developer" | "teacher";
-
-interface User {
-  name: string;
-  job: Job;
-}
-
-const user: User = {
-  name: "Bob",
-  job: "developer",
-};
-
-//Union Types
-
-// name이 식별 가능한 union type이라고 함
 interface Car {
-  name: "car";
-  color: string;
+  name: string;
   start(): void;
 }
 
-interface Mobile {
-  name: "mobile";
+interface Toy {
+  name: string;
   color: string;
-  call(): void;
+  price: number;
 }
 
-function getGift(gift: Car | Mobile) {
-  // 둘다 있는 속상 깂 이기 때문에 오류 x
-  console.log(gift.color);
-
-  // 둘다 있는 속성값이 아니기 때문에 오류 o
-  // gift.start()
-
-  // 오류 x
-  if (gift.name === "car") {
-    gift.start();
-  } else {
-    gift.call();
-  }
-}
+const toyCar: Toy & Car = {
+  name: "타요",
+  start() {},
+  color: "blue",
+  price: 1000,
+};
