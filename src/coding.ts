4b57ecb48dee505716426707db4435c0b231b7f1
hyojoo <jhj080802@naver.com>
2024-08-19 19:55:15 +0900

    record

--- src/coding.ts
+++ src/coding.ts
@@ -1,15 +1,34 @@
-// ReadOnly
+// Record<K,T>
+// K = key
+// T = type
+
+// interface Score  {
+//   "1": "A"|"B"|"C"|"D";
+//   "2": "A"|"B"|"C"|"D";
+//   "3": "A"|"B"|"C"|"D";
+//   "4": "A"|"B"|"C"|"D";
+// }
+
+// type Grade = "1" | "2" | "3" | "4";
+// type Score = "A" | "B" | "C" | "D" | "F";
+// const score: Record<Grade, Score> = {
+//   1: "A",
+//   2: "B",
+//   3: "C",
+//   4: "D",
+// };
 
 interface User {
   id: number;
   name: string;
-  age?: number;
+  age: number;
 }
 
-let admin: Readonly<User> = {
-  id: 1,
-  name: "Bob",
-};
-
-//error
-admin.id = 4;
+function isVaild(user: User) {
+  const result: Record<keyof User, boolean> = {
+    id: user.id > 0,
+    name: user.name !== "",
+    age: user.age > 0,
+  };
+  return result;
+}
