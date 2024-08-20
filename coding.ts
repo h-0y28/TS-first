a9c7ce6262dad6a16e2874fc880fb62ea57dcd5b
hyojoo <jhj080802@naver.com>
2024-08-20 22:57:34 +0900

    generic->any

--- coding.ts
+++ coding.ts
@@ -1,11 +1,11 @@
-type SuperPrint = {
-  // return type도 T로 지정
-  <TypePlaceholder>(arr: TypePlaceholder[]): TypePlaceholder;
-};
+type SuperPrint = (a: any[]) => any;
 
 const superPrint: SuperPrint = (arr) => arr[0];
 
-const a = superPrint([1, 2, 3, 4]);
-const b = superPrint([true, false, true]);
-const c = superPrint(["1", "2", "3"]); // c: string
-const d = superPrint([1, false, true, 4, "hello"]); // d: string | number | boolean
+const a = superPrint([1, 2, 3, 4]); // a: any
+const b = superPrint([true, false, true]); // b: any
+const c = superPrint(["1", "2", "3"]); // c: any
+const d = superPrint([1, false, true, 4, "hello"]); // d: any
+
+// 보로를 받지 못 함 -> 배열의 첫번째 요소를 리턴해줌 = number
+d.toUpperCase();
