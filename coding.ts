acd28db35f74f7bdf649a2b0ae97e2b635c160d4
hyojoo <jhj080802@naver.com>
2024-08-20 20:49:10 +0900

    overloading

--- coding.ts
+++ coding.ts
@@ -1,9 +1,33 @@
-function add(a: number, b: number): number {
+type Config = {
+  path: string;
+  state: object;
+};
+
+type Push = {
+  (path: string): void;
+  (config: Config): void;
+};
+
+const push: Push = (config) => {
+  if (typeof config === "string") {
+    console.log(config);
+  } else {
+    console.log(config.path);
+  }
+};
+
+//
+
+type Add = {
+  (a: number, b: number): number;
+  (a: number, b: number, c: number): number;
+};
+
+// 실제 함수 구현부에서 optional인지 정의함
+const add: Add = (a, b, c?: number) => {
+  if (c) return a + b + c;
   return a + b;
-}
+};
 
-const add2 = (a: number, b: number) => a + b;
-
-type Add = (a: number, b: number) => number;
-
-const add3: Add = (a, b) => a + b;
+add(1, 2);
+add(1, 2, 3);
