64bfc920f4152f1930ea659669081fc12a8ac8f2
hyojoo <jhj080802@naver.com>
2024-08-21 15:03:03 +0900

    interface vs type

--- coding.ts
+++ coding.ts
@@ -1,32 +1,20 @@
-type Words = {
-  [key: string]: string;
+type Team = "red" | "blue" | "yellow";
+type Health = 1 | 5 | 10;
+
+interface Player {
+  nickname: string;
+  team: Team;
+  health: Health;
+}
+
+type Player = {
+  nickname: string;
+  team: Team;
+  health: Health;
 };
 
-class Dict {
-  private words: Words;
-  constructor() {
-    this.words = {};
-  }
-  add(word: Word) {
-    if (this.words[word.term] === undefined) {
-      this.words[word.term] = word.def;
-    }
-  }
-  def(term: string) {
-    return this.words[term];
-  }
-  static hello() {
-    return "hello";
-  }
-}
-
-class Word {
-  constructor(public readonly term: string, public readonly def: string) {}
-}
-
-const kimchi = new Word("kinchi", "한국의 음식");
-
-const dict = new Dict();
-
-dict.add(kimchi);
-dict.def("kimchi");
+const nico: Player = {
+  nickname: "nico",
+  team: "blue",
+  health: 5,
+};
